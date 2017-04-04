67f9301712fa6a95da843bafd2a0cda307b24b76
ng887 <nehagupta887@gmail.com>
2017-04-04 14:04:10 -0700

    Google places Api demo

---
+++ ProjectWebDevTrial/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+    class App extends Component {
+    componentDidMount() {
+        /* global google b:true */
+        const input = document.getElementById('autocomplete')
+        this.autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'] })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+
+                    <input id="autocomplete"/>
+                </p>
+            </div>
+        )
+    }
+}
+export default App;
